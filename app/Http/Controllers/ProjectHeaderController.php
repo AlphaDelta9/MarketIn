9518fd0899add7bb2bb5929152894aef252a138f
AlphaDelta9 <AlphaDelta9@users.noreply.github.com>
2022-11-16 19:51:21 +0700

    Head commit

---
+++ app/Http/Controllers/ProjectHeaderController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProjectHeader;
+use Illuminate\Http\Request;
+
+class ProjectHeaderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProjectHeader  $projectHeader
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProjectHeader $projectHeader)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProjectHeader  $projectHeader
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProjectHeader $projectHeader)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\Request  $request
+     * @param  \App\Models\ProjectHeader  $projectHeader
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProjectHeader $projectHeader)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProjectHeader  $projectHeader
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProjectHeader $projectHeader)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_11_15_132106_create_project_headers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectHeadersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_headers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_headers');
+    }
+}
