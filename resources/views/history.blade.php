3c4ba4cbddbf98bcabd68f47024d0fa9df195004
AlphaDelta9 <AlphaDelta9@users.noreply.github.com>
2023-01-08 20:01:31 +0700

    Table field

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -3,14 +3,14 @@
 
 @isset($logA)
 @foreach ($logA as $log)
-    <a href={{secure_url("project/{$log->projectHeader->id}")}}>
+    <a href={{secure_url("project/{{$log->projectHeader->id}}")}}>
         {{$log->projectHeader->title}}
     </a>
 @endforeach
 @endisset
 @isset($logP)
 @foreach ($logP as $log)
-    <a href={{secure_url("project/{$log->id}")}}>
+    <a href={{secure_url("project/{{$log->id}}")}}>
         {{$log->title}}
     </a>
 @endforeach
