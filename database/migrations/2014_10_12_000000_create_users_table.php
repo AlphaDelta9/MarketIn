a03fa6691830dfc39a05f9b8ab4529fd5336d30c
AlphaDelta9 <AlphaDelta9@users.noreply.github.com>
2023-01-23 12:49:33 +0700

    Revert "Merge branch 'debug' of https://github.com/AlphaDelta9/CROWDWORKING into debug"
    This reverts commit 4b73d79d99df3a3f52ea1219a12269635125bcaf, reversing
    changes made to e3a7062a114ffd333e510acd10b0b338693de032.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,7 +24,6 @@ class CreateUsersTable extends Migration
             $table->binary('picture');
             $table->rememberToken();
             $table->timestamps();
-            $table->text('address');
         });
     }
 
